2afc023fb09dd5c17aa13d0a098771f608a62b56
Opilane TTHK <piritamarcus@gmail.com>
2024-09-23 14:17:57 +0300

    23.09.24

---
+++ SeleniumCsharp/SeleniumCsharp/driver/Test-attributes.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+[Test]
+public void test_search()
+{
+    driver.Url = test_url;
+    System.Threading.Thread.Sleep(2000);
+    IWebElement searchText = driver.FindElement(By.CssSelector("[name = 'q']"));
+
+    searchText.SendKeys("LambdaTest");
+
+    ............................................................................
+}
---
+++ SeleniumCsharp/SeleniumCsharp/driver/setup-attribute.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium.Chrome;
+
+[SetUp]
+public void start_Browser()
+{
+    // Local Selenium WebDriver
+    driver = new ChromeDriver();
+    driver.Manage().Window.Maximize();
+}
