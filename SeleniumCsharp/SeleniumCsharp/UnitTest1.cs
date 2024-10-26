7bc3cf7a5b774f6a48944692f9fbdfd2e36dffa7
MarcusPirita <piritamarcus@gmail.com>
2024-10-26 17:35:09 +0300

    selenium

--- SeleniumCsharp/SeleniumCsharp/UnitTest1.cs
+++ SeleniumCsharp/SeleniumCsharp/UnitTest1.cs
@@ -1,16 +1,37 @@
-namespace SeleniumCsharp
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+using System;
+using Xunit; 
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+public class SeleniumTests : IDisposable
+{
+    private readonly IWebDriver driver;
+
+    public SeleniumTests()
+    {
+        driver = new ChromeDriver(); 
+    }
+
+    public void Dispose()
+    {
+        driver.Quit(); 
+    }
+
+    [Fact]
+    public void TestFirstWebsite()
+    {
+        driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+        Xunit.Assert.Contains("Wikipedia", driver.Title);
+        var element = driver.FindElement(By.Id("search"));
+        Xunit.Assert.True(element.Displayed);
+    }
+
+    [Fact]
+    public void TestSecondWebsite()
+    {
+        driver.Navigate().GoToUrl("https://www.err.ee/");
+        Xunit.Assert.Contains("ERR", driver.Title);
+        var anotherElement = driver.FindElement(By.ClassName("site-logo"));
+        Xunit.Assert.True(anotherElement.Displayed);
     }
 }
--- SeleniumCsharp/SeleniumCsharp/driver/setup-attribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenQA.Selenium.Chrome;
-
-[SetUp]
-public void start_Browser()
-{
-    // Local Selenium WebDriver
-    driver = new ChromeDriver();
-    driver.Manage().Window.Maximize();
-}
